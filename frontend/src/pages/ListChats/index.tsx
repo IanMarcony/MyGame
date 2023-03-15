4963544b92c124d9c344a0afab02c6af9c62f795
Ian Salazar <ian.salazar@itriad.org.br>
2023-03-15 11:10:04 -0400

    feat: add new page to list chats

---
+++ frontend/src/pages/ListChats/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ListChats: React.FC = () => {
+  return (
+    <Container>
+      <h1>Lista chatss</h1>
+    </Container>
+  );
+};
+
+export default ListChats;
---
+++ frontend/src/pages/ListChats/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 10px;
+  margin: 0 auto;
+  width: 60%;
+`;
